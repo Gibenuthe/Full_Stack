0148da2d6cfc224f89aa5e7b4f5d275356b30bc9
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2022-06-11 10:50:07 -0300

    Initial commit

---
+++ node-express-mongodb-master/app/config/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //url: "mongodb://localhost:27017/bezkoder_db"
+  url: "mongodb+srv://Trab_refugiado:Ucrania123@cluster0.uw1pvuu.mongodb.net/AcolhedoresDB?retryWrites=true&w=majority"
+  //mongodb+srv://Trab_refugiado:Ucrania123@cluster0.uw1pvuu.mongodb.net/AcolhedoresDB?retryWrites=true&w=majority
+};
